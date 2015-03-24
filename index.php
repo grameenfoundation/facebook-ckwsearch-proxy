b53a17ea2b02ec297bd35ecefe17714511293df7
Herbert Kalule <kaluleherbert@gmail.com>
2015-03-23 19:19:18 -0500

    index file

--- index.php
+++ index.php
@@ -36,8 +36,8 @@
             $id = "";
         }
         //select  Menu Item where id $_GET["id"]
-        $subMenuquery = ("select * from menu_item where parent_id='$id' order by position asc, label asc");
-        $subMenuResult = mysqli_query($connection, $subMenuquery)
+        $subMenuQuery = ("select * from menu_item where parent_id='$id' order by position asc, label asc");
+        $subMenuResult = mysqli_query($connection, $subMenuQuery)
                 or die('Invalid query for selecting getMenuItemLabel: ' . mysqli_error($connection));
         echo '' . "</br>";
         while ($subMenuObj = $subMenuResult->fetch_object()) {
@@ -45,20 +45,19 @@
             if (strpos($subMenuContent, 'No Content') !== false) {
                 $subMenuContent = '';
             }
-            //check if Id not hidden arraylist
+            //check if Id not hidden in arraylist
             if (!in_array($subMenuObj->id, $hiddenIDarray)) {
 
-                //check if id in last level tree
                 //select all last level IDS in the tree for CKW Search
                 $subId = $subMenuObj->id;
-                $query = ("select count(id) as No from menu_item where parent_id='$subId'");
+                $query = ("select count(id) as no from menu_item where parent_id='$subId'");
                 $result = mysqli_query($connection, $query)
                         or die('Invalid query for selecting parentId: ' . mysqli_error($connection));
                 $countObj = $result->fetch_object();
-                $no = $countObj->No;
-
-
-                if ($no == '0') {
+                $count = $countObj->no;
+               
+                //check if id count is for last level tree
+                if ($count == '0') {
                     echo "" . "<ul id='menu'>";
                     echo "" . "<li><b>" . $subMenuObj->label . "</b></li>";
                     echo "" . $subMenuContent . "</ul> ";
@@ -71,8 +70,8 @@
             }
         }
         echo '' . "</br>";
+        echo '' . "Copyright (c) 2015 Grameen Foundation</br>";
         
-        echo 'Copyright (c) 2015 Grameen Foundation';
     } else {
 
         die('Error connecting to Db' . mysqli_error($connection));
